4c87e48be914aa4ba01544b50fe1c404848a43f1
unknown <telesforociriaco@gmail.com>
2016-07-04 22:04:29 -0500

    agregar por el usuario al array

--- js/funcionciclo.js
+++ js/funcionciclo.js
@@ -1,5 +1,21 @@
 var lista = ["coca","pepsi","manzanita","sevenup","sprite","caballito"];
 
+lista.push(prompt("Introduce el nuevo articulo"));
+document.write(lista[lista.length-1] + "</p>");
+document.write("<p> El primer articulo es ");
+document.write(lista[2] + "</p>");
+document.write("<p> El ultimo es articulo es ");
+document.write(lista[lista.length-1] + "</p>");
+lista.unshift("fanta");
+document.write("<p> El nuevo articulo es: ");
+document.write(lista[0] + "</p>");
+
+
+//alert("borraremos el primer articulo");
+//lista.shift();
+//alert("borraremos el ultimo articulo");
+//lista.pop();
+
 
 i = 0;
 lista.sort();
@@ -17,3 +33,4 @@ myList = document.getElementById("results");
 		myList.appendChild(li);
 
 }
+
