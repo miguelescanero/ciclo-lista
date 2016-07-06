3c8a271433ee9fbf33ce371d13f87c11df9fbb95
unknown <telesforociriaco@gmail.com>
2016-07-05 22:49:33 -0500

    agrega elemntos y borra elemntos y ordena

--- js/funcionciclo.js
+++ js/funcionciclo.js
@@ -1,52 +1,43 @@
 var lista = ["coca","pepsi","manzanita","sevenup","sprite","caballito"];
 
-
-
-
-
-//document.write("<p> El primer articulo es ");
-//document.write(lista[2] + "</p>");
-//document.write("<p> El ultimo es articulo es ");
-//document.write(lista[lista.length-1] + "</p>");
-//lista.unshift("fanta");
-//document.write("<p> El nuevo articulo es: ");
-//document.write(lista[0] + "</p>");
-
-
-//alert("borraremos el primer articulo");
-//lista.shift();
-//alert("borraremos el ultimo articulo");
-//lista.pop();
-
-
-
 i = 0;
 lista.sort();
 var myList;
 var li;
-function imprime(){
-//document.write(lista);
-		myList = document.getElementById("results");
-		for(i = 0; i <lista.length; i++){
+
+myList = document.getElementById("results");
+imprime(lista);
+function imprime(arreglo){
+//document.write(arreglo);
+		myList.innerHTML = "";
+		for(i = 0; i <arreglo.length; i++){
 		li = document.createElement("li");
-		li.innerHTML=lista[i];
+		li.innerHTML=arreglo[i];
 		myList.appendChild(li);
+		//li.setAttribute("onclick","eliminar()");
+		li.onclick = eliminar;
 		}
 }
-	function agregar(){
+function agregar(){
 		lista.push(prompt("Introduce el nuevo articulo"));
-			myList = document.getElementById("results");
 			for(i = i; i <lista.length; i++){
 			li = document.createElement("li");
 			li.innerHTML=lista[i];
 			myList.appendChild(li);
+			li.setAttribute("onclick","eliminar()");
 			}
 			
 	}
-
-	function ordenar(){
-		myList.sort();
-		imprime(myList);
-
-	}
+var lista1;
 	
+
+function ordenar(){
+	imprime( lista.sort() );
+}
+
+
+function eliminar(){
+		console.log(this);
+		this.setAttribute("style","color:green");
+		myList.removeChild(this);
+}
